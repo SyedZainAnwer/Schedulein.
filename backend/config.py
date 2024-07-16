a0ec2c08c7416a372e9cf180bd2fce337a6714c0
Syed Zain Anwer <zainanwer24@gmail.com>
2024-07-16 14:28:00 +0200

    first commit

---
+++ backend/config.py
@@ -0,0 +1,8 @@
+import os
+
+class Config:
+    DB_NAME = os.environ.get("DB_NAME")
+    DB_PASS = os.environ.get("DB_PASS")
+    DB_HOST = os.environ.get("DB_HOST")
+    DB_USER = os.environ.get("DB_USER")
+    DB_PORT = os.environ.get("DB_PORT")
