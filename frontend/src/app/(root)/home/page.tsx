5df1df9737289cbb2cb25e43aa4196542eea7e0f
Syed Zain Anwer <zainanwer24@gmail.com>
2024-07-27 00:44:58 +0200

    create home page

---
+++ frontend/src/app/(root)/home/page.tsx
@@ -0,0 +1,24 @@
+import LeftSideBar from "@/components/LeftSideBar";
+
+const page = () => {
+    return(
+        <div className="flex w-full">
+            {/* Left Side Bar */}
+            <section className="w-1/5 bg-slate-100 h-screen">
+                <LeftSideBar />
+            </section>
+
+            {/* Main Task Display Area */}
+            <section className="w-4/5">
+                <p>Tasks</p>
+            </section>
+
+            {/* Graph Area */}
+            <section className="w-1/5">
+                <p>Graphs</p>
+            </section>
+        </div>
+    )
+}
+
+export default page;
---
+++ frontend/src/components/LeftSideBar.tsx
@@ -0,0 +1,31 @@
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/dropdown-menu";
+
+const LeftSideBar = () => {
+    return (
+        <div>
+            {/* Top Profile Section */}
+            <div className="">
+                <Avatar>
+                    <AvatarImage src="https://github.com/shadcn.png" />
+                    <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+
+                <DropdownMenu>
+                    <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+                    <DropdownMenuContent>
+                        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>Profile</DropdownMenuItem>
+                        <DropdownMenuItem>Billing</DropdownMenuItem>
+                        <DropdownMenuItem>Team</DropdownMenuItem>
+                        <DropdownMenuItem>Subscription</DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+
+            </div>
+        </div>
+    )
+}
+
+export default LeftSideBar;
